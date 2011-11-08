c58826ae953b49acad1ea41abf860d2a07e3673d
Tyler Montgomery <tyler.a.montgomery@gmail.com>
2011-11-08 16:06:31 -0700

    renamed coffeescript files for clarity

--- lib/handlebars_routes.rb
+++ lib/handlebars_routes.rb
@@ -2,4 +2,10 @@ require "handlebars_routes/version"
 require 'handlebars_routes/engine'
 
 module HandlebarsRoutes
+  def self.assets_path
+    puts "="*45
+    puts 'omg!'
+    puts "="*45
+    @assets_path ||= File.expand_path('../../vendor/assets/', __FILE__)
+  end
 end
